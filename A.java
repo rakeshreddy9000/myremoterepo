7e14c8b5ecf46d4839dc1c87835d676e58fbe42b
rakeshreddy9000 <rakeshreddyr532@gmail.com>
2024-02-14 19:41:02 +0530

    modify A.java and crreated C.java

--- A.java
+++ A.java
@@ -1,7 +1,7 @@
 public class A{
 public static void main(String args[]){
 
-System.out.println("java is oops");
+System.out.println("java is awesome");
 }
 
 }
--- B.java
+++ B.java
@@ -1,7 +1,7 @@
 public class A{
 public static void main(String args[]){
 
-System.out.println("class");
+System.out.println("class B");
 }
 
 }
